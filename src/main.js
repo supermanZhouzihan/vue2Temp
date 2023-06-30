e9d1a42049788bc17d5d9d93652232224de95f28
zhouzihan <350636553@qq.com>
2023-06-30 13:06:03 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,64 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/styles/common.scss' // common css
+
+import moment from "moment";
+import {
+  formatDate
+} from "@/utils/generator/dateTimeConfig.js";
+
+import {
+  Button,
+  Divider,
+  Col,
+  Row,
+  Dialog,
+  NavBar,
+  Cell,
+  Icon,
+  CellGroup,
+  Badge ,
+  Toast,
+  ActionSheet,
+  Form,
+  Field,
+  Popup,
+  DatetimePicker,
+  SwipeCell,
+  List,
+  Sticky
+} from 'vant';
+
+
+//挂载全局组件
+Vue.use(Button);
+Vue.use(Divider);
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(NavBar);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(Icon);
+Vue.use(Badge);
+Vue.use(ActionSheet);
+Vue.use(Form);
+Vue.use(Field);
+Vue.use(Popup);
+Vue.use(DatetimePicker);
+Vue.use(SwipeCell);
+Vue.use(List);
+Vue.use(Sticky);
+Vue.use(Toast);
+Vue.use(Dialog);
+
+//挂载全局方法
+Vue.prototype.$moment = moment;
+Vue.prototype.$formatDate = formatDate;
+moment.locale('zh-cn')
+
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,45 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+  },
+  {
+    path: '/index',
+    name: 'Index',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/index.vue')
+  },
+  {
+    path: '/inventroy',
+    name: 'Inventory',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/inventory.vue')
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/savedOrder',
+    name: 'savedOrder',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/savedOrder.vue')
+  },
 ]
 
 const router = new VueRouter({
